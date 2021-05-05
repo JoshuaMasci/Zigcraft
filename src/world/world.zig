4831306fc82163a00bba5af5161c5e8285d0f86f
Joshua_Masci <joshuamasci1@gmail.com>
2021-05-04 21:59:53 -0700

    Reworked chunk matrix

--- src/world/world.zig
+++ src/world/world.zig
@@ -6,6 +6,7 @@ usingnamespace @import("../transform.zig");
 usingnamespace @import("../chunk/chunk.zig");
 const c = @import("../c.zig");
 const opengl = @import("../opengl_renderer.zig");
+const glfw = @import("../glfw_platform.zig");
 
 //TODO 3D chunk
 pub const ChunkPos = Vec2(i32);
@@ -16,7 +17,7 @@ const ChunkInfo = struct {
     coord: ChunkPos,
     data: ChunkData32,
     mesh: opengl.Mesh,
-    transform: Transform,
+    matrix: mat4,
 
     pub fn deinit(self: *Self) void {
         self.mesh.deinit();
@@ -32,18 +33,24 @@ pub const World = struct {
     pub fn init(allocator: *std.mem.Allocator) Self {
         var chunk_map = std.AutoHashMap(ChunkPos, ChunkInfo).init(allocator);
 
-        var new_chunk_data = ChunkData32.init();
-        generateChunk(&new_chunk_data);
+        const CHUNK_SIZE = 10;
+        var index = ChunkPos.zero();
+        while (index.x < CHUNK_SIZE) : (index.x += 1) {
+            index.y = 0;
+            while (index.y < CHUNK_SIZE) : (index.y += 1) {
+                var new_chunk_data = ChunkData32.init();
+                generateChunk(&new_chunk_data);
 
-        var new_chunk = ChunkInfo {
-            .coord = ChunkPos.zero(),
-            .data = new_chunk_data,
-            .mesh = CreateChunkMesh(ChunkData32, allocator, &new_chunk_data),
-            .transform = Transform.zero(),
-        };
-        new_chunk.data.dirty = false;
-
-        chunk_map.put(new_chunk.coord, new_chunk) catch panic("Failed to put", .{});
+                var new_chunk = ChunkInfo {
+                    .coord = index,
+                    .data = new_chunk_data,
+                    .mesh = CreateChunkMesh(ChunkData32, allocator, &new_chunk_data),
+                    .matrix = mat4.from_translate(vec3.new(@intToFloat(f32, @intCast(u64, index.x) * ChunkData32.size_x), 0.0, @intToFloat(f32, @intCast(u64, index.y) * ChunkData32.size_z))),
+                };
+                new_chunk.data.dirty = false;
+                chunk_map.put(new_chunk.coord, new_chunk) catch panic("Failed to put", .{});
+            }
+        }
 
         return Self {
             .allocator = allocator,
@@ -71,24 +78,13 @@ pub const World = struct {
                 entry.value.data.dirty = false;
             }
         }
-
-        // var new_chunk_data = ChunkData32.init();
-        // var new_chunk = ChunkInfo {
-        //     .coord = ChunkPos.zero(),
-        //     .data = new_chunk_data,
-        //     .mesh = CreateChunkMesh(ChunkData32, self.allocator, &new_chunk_data),
-        //     .transform = Transform.zero(),
-        // };
-
-        //self.chunk_map.put(ChunkPos.new(@intCast(i32, self.chunk_map.count()), 0), new_chunk) catch panic("Failed to put", .{});
     }
 
     pub fn render(self: *Self, matrix_uniform_index: c.GLint) void {
         var iterator = self.chunk_map.iterator();
         while (iterator.next()) |entry| {
             //Model Matrix
-            var model_matrix = entry.value.transform.getModelMatrix();
-            c.glUniformMatrix4fv(matrix_uniform_index, 1, c.GL_FALSE, model_matrix.get_data());
+            c.glUniformMatrix4fv(matrix_uniform_index, 1, c.GL_FALSE, entry.value.matrix.get_data());
             entry.value.mesh.draw();
         }
     }
